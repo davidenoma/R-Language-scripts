5b9f789dd64e36e9a70db6c93be5dfb7a23b8241
David Enoma <davidenoma@gmail.com>
2022-07-20 21:48:58 +0100

    addtion of kgp to rsid snps

--- convert_kgp_to_rsid.R
+++ convert_kgp_to_rsid.R
@@ -10,6 +10,7 @@ unique_chroms = unique(gen_cord$V1)
   for (variable in unique_chroms) {
     if (nchar (variable) == 4) {
        gen_cord$V1[gen_cord$V1 == variable] <- str_sub(variable,-1,-1)
+       
       
     }else{
       gen_cord$V1[gen_cord$V1 == variable] <- str_sub(variable,-2,-1)
@@ -20,6 +21,11 @@ coords <- apply(gen_cord, 1, paste, collapse = ":")
 
 final_cord <- getBM(attributes = c('refsnp_id', 'chr_name', 'chrom_start', 'chrom_end', 'allele'),
       filters = c('chromosomal_region'), 
-      values = coords,
+      values = coords[1:500],
       verbose = TRUE,
       mart = snpMart) 
+
+#Reading in sumstats
+sum_stats_gemma = read.csv("C:/Users/HP/OneDrive/Desktop/david_sum_stats_gemma.assoc.txt")
+
+sum_stats_filter_p_values = sum_stats_gemma[sum_stats_gemma$p_wald < 0.05,]
